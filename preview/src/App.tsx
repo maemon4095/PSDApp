57fbe7304e4c21add2eda12f771b13c45d926eca
maemon4095 <maemondecocted@gmail.com>
2024-04-29 22:20:29 +0900

    resizebox

--- preview/src/App.tsx
+++ preview/src/App.tsx
@@ -1,76 +1,23 @@
 import { useEffect, useRef, useState } from "preact/hooks";
-import { ComponentChildren, h } from "preact";
-import Progress from "~/components/Progress.tsx";
+import { ComponentChildren, Fragment, h } from "preact";
+import Partitioned from "~/components/Partitioned.tsx";
+import ResizeBox from "./ResizeBox.tsx";
+
 export default function App() {
   return (
     <div class="size-full flex flex-col items-stretch">
       <div class="flex-1 grid place-content-stretch">
         <ResizeBox>
-          <Progress progress={0.5} status="yo"></Progress>
+          <Partitioned direction="column">
+            <Partitioned direction="row">
+              <div class="bg-blue-400" />
+              <div class="bg-red-400" />
+              <div class="bg-green-400" />
+            </Partitioned>
+            <div class="bg-yellow-400" />
+          </Partitioned>
         </ResizeBox>
       </div>
-    </div>
-  );
-}
-
-function ResizeBox({ children }: { children?: ComponentChildren }) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [pos, setPos] = useState({
-    x0: 0,
-    y0: 0,
-    x1: 0,
-    y1: 0,
-  });
-  useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-    const x0 = container.clientWidth * 0.25;
-    const y0 = container.clientHeight * 0.25;
-    const x1 = container.clientWidth * 0.75;
-    const y1 = container.clientHeight * 0.75;
-    setPos({ x0, y0, x1, y1 });
-  }, []);
-
-  return (
-    <div
-      ref={containerRef}
-      class="translate-x-0"
-    >
-      <div
-        style={{
-          left: Math.min(pos.x0, pos.x1),
-          top: Math.min(pos.y0, pos.y1),
-          width: Math.abs(pos.x1 - pos.x0),
-          height: Math.abs(pos.y1 - pos.y0),
-        }}
-        class="resize-box"
-      >
-        {children}
-      </div>
-      <span
-        onPointerMove={(e) => {
-          if (e.buttons !== 1) return;
-          setPos({
-            ...pos,
-            x0: pos.x0 + e.movementX,
-            y0: pos.y0 + e.movementY,
-          });
-        }}
-        class="resize-box-handle"
-        style={{ left: pos.x0, top: pos.y0 }}
-      />
-      <span
-        onPointerMove={(e) => {
-          if (e.buttons !== 1) return;
-          setPos({
-            ...pos,
-            x1: pos.x1 + e.movementX,
-            y1: pos.y1 + e.movementY,
-          });
-        }}
-        class="resize-box-handle"
-        style={{ left: pos.x1, top: pos.y1 }}
-      />
     </div>
   );
 }
---
+++ src/components/Partitioned.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useMemo, useRef, useState } from "preact/hooks";
+import { ComponentChildren, Fragment, h, toChildArray } from "preact";
+
+type Direction = "row" | "column";
+
+const handleSize = 13;
+
+export default function Partitioned(
+  props: {
+    firstSize?: number;
+    direction: Direction;
+    children?: ComponentChildren;
+  },
+) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [dragOffset, setDragOffset] = useState(null as null | [number, number]);
+  const [firstSize, setFirstSize] = useState(props.firstSize ?? 0);
+
+  const { first, last, handleArea } = useMemo(() => {
+    const [first, ...lasts] = toChildArray(props.children);
+    if (lasts.length === 0) {
+      return { first, last: undefined, handleArea: 0 };
+    }
+    if (lasts.length === 1) {
+      return { first, last: lasts[0], handleArea: handleSize };
+    }
+    return {
+      handleArea: handleSize * lasts.length,
+      first,
+      last: <Partitioned direction={props.direction}>{lasts}</Partitioned>,
+    };
+  }, [props.children]);
+
+  const contents = (() => {
+    if (firstSize === undefined) {
+      return <div class="child-full flex-1">{first}</div>;
+    }
+    return (
+      <>
+        <div
+          class="child-full"
+          style={{
+            flexBasis: `${firstSize}px`,
+          }}
+        >
+          {first}
+        </div>
+        <Handle
+          dir={props.direction}
+          onPointerDown={(e) => {
+            setDragOffset([e.offsetX, e.offsetY]);
+          }}
+        />
+        <div class="child-full flex-1">
+          {last}
+        </div>
+      </>
+    );
+  })();
+
+  const onPointerMove = (e: PointerEvent) => {
+    e.preventDefault();
+    if (!dragOffset) return;
+    const container = containerRef.current;
+    if (!container) return;
+    const origin = container.getBoundingClientRect();
+    const [offsetX, offsetY] = dragOffset;
+
+    if (props.direction === "row") {
+      const x = Math.floor(e.clientX - origin.x - offsetX);
+      setFirstSize(Math.min(x, origin.width - handleArea));
+    } else {
+      const y = Math.floor(e.clientY - origin.y - offsetY);
+      setFirstSize(Math.min(y, origin.height - handleArea));
+    }
+  };
+
+  const onPointerUp = (e: PointerEvent) => {
+    setDragOffset(null);
+  };
+
+  useEffect(() => {
+    document.addEventListener("pointerup", onPointerUp);
+    return () => document.removeEventListener("pointerup", onPointerUp);
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      onPointerMove={onPointerMove}
+      class="flex items-stretch"
+      style={{ flexDirection: props.direction }}
+    >
+      {contents}
+    </div>
+  );
+}
+
+function Handle(
+  props: {
+    dir: Direction;
+    onPointerDown?: (e: h.JSX.TargetedPointerEvent<HTMLDivElement>) => void;
+  },
+) {
+  const { dir } = props;
+  const cursor = dir === "row" ? "col-resize" : "row-resize";
+
+  const style: h.JSX.CSSProperties = {
+    flex: `0 0 ${handleSize}px`,
+    flexDirection: dir,
+    cursor,
+  };
+
+  return (
+    <div
+      onPointerDown={(e) => {
+        e.preventDefault();
+        props.onPointerDown?.(e);
+      }}
+      class="flex justify-center"
+      style={style}
+    >
+      <div
+        class="bg-slate-500 self-stretch pointer-events-none"
+        style={{ flex: "0 1px" }}
+      />
+    </div>
+  );
+}
