be06b67b5d0448d736b2f95ebd6494a90d07f8d2
maemon4095 <maemondecocted@gmail.com>
2024-05-22 10:21:39 +0900

    use switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import Home from "~/pages/Home.tsx";
 import PSDView from "~/pages/PSDView.tsx";
-import { createSwitcher } from "~/components/Switcher.tsx";
+import { createSwitcher } from "jsr:@maemon4095/preact-switcher";
 import Layout from "~/layout/default.tsx";
 
 const paths = {
