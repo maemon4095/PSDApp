d790bff707befbe0ad2119d1b15044d87de22b6b
maemon4095 <maemondecocted@gmail.com>
2024-05-31 21:40:29 +0900

    make service worker optional

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import "./types/meta.d.ts";
 import { render } from "preact";
 import App from "./App.tsx";
-// @deno-types=@loader-types/file.d.ts
-import worker from "./service.worker.ts";
 
-if (!import.meta.isDev) {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(worker, { scope: "." });
-  }
-}
 render(
   <App />,
   document.body,
